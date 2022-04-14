72e3650db44bbe0febbcf264676d417bd8565ecd
dlxorb96 <xorb_1@naver.com>
2022-04-14 17:48:59 +0900

    2022.04.14

--- src/main/java/com/example/entity/ItemEntity.java
+++ src/main/java/com/example/entity/ItemEntity.java
@@ -12,8 +12,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.format.annotation.DateTimeFormat;
 
